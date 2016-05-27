3d8557e6a3dc5b3426ace7dfc43312c2bb431084
moticon <soonerpat@gmail.com>
2016-05-27 10:57:22 -0500

    Added a second class "TwoNumbers" to test commit and try and figure out why this doesn't seem to be pushing anything to GitHub.

---
+++ app/src/main/java/com/moticon/threenumbers/TwoNumbers.java
@@ -0,0 +1,14 @@
+package com.moticon.threenumbers;
+
+/**
+ * Created by moticon on 5/27/2016.
+ */
+public class TwoNumbers {
+    Integer a;
+    Integer b;
+
+    public TwoNumbers(Integer a, Integer b){
+        this.a = a;
+        this.b = b;
+    }
+}
